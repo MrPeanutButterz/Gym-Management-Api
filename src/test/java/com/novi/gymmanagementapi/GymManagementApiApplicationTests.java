836b64e6f328398ca08f96555a5c6e63f9d18e6d
Charlie Eysbroek <C.eysbroek@hotmail.com>
2023-11-07 22:32:51 +0100

    new project

---
+++ src/test/java/com/novi/gymmanagementapi/GymManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novi.gymmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
